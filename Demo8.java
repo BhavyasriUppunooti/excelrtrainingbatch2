3ebd6a5df252984a6f76bffc635cac2829e7b89a
BhavyasriUppunooti <ubhavyasri01@gmail.com>
2025-01-04 11:16:17 +0530

    Update Demo8.java

--- Demo8.java
+++ Demo8.java
@@ -1,22 +1,27 @@
 import java.util.Scanner;
-public class Demo8{
-    public static void main(String[] args){
+
+public class Demo8 {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String loginid;
-        String pwd;
-        string reply;
-        do{
-            System.out.println("Welcome to malla reddy university");
-            System.out.println("Have you visited site earlier");
-            String reply = sc.nextLine();
-            if(reply == "no"){
-                System.out.println("login here");
-                System.out.println("enter ur id and pwd");
-                 String loginid = sc.nextLine();
-                 String pwd = sc.nextLine();
-            } else
-            System.out.println("your are our valued customer");
-        }while((loginid == 'bhavya') && (pwd == "user001"));
+        String loginid = "";
+        String pwd = "";
+        String reply;
+
+        do {
+            System.out.println("Welcome to Malla Reddy University");
+            System.out.println("Have you visited the site earlier?");
+            reply = sc.nextLine();
+
+            if (reply.equals("no")) {
+                System.out.println("Login here");
+                System.out.println("Enter your id and password");
+                loginid = sc.nextLine();
+                pwd = sc.nextLine();
+            } else {
+                System.out.println("You are our valued customer");
             }
-        }
-    
+        } while (!(loginid.equals("bhavya") && pwd.equals("user001")));
+
+        System.out.println("Login successful!");
+    }
+}

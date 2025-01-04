0e62164b42b880cfba8282f47271abf7381bcca8
BhavyasriUppunooti <ubhavyasri01@gmail.com>
2025-01-04 11:12:30 +0530

    Add files via upload

---
+++ Demo5.java
@@ -0,0 +1,10 @@
+public class Demo5{
+    public static void main(String[] args){
+        String username = "username";
+        String pwd = "user_001";
+        while((username == "username") && (pwd == "user_001")){
+            System.out.println("hello user happy new year");
+            break;
+        }
+    }
+}

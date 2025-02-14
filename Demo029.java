49bfa98bd6f33ed683718dc19833371d78fca27c
BhavyasriUppunooti <ubhavyasri01@gmail.com>
2025-02-14 22:13:01 +0530

    Add files via upload

---
+++ Demo029.java
@@ -0,0 +1,8 @@
+public class Demo029 {
+    public static void main(String[] args) {
+        String[] frnds = { "Ah", "sk", "sd", "sk", "yi", "mh", "si", "vk", "si" };
+        for (String friend : frnds) {
+            System.out.println(friend);
+        }
+    }
+}
